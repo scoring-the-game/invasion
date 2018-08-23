c8c658f11aa9db42579cbf6e7b6c0ae64bc597fe
Michael Cohen <michael@planetcohen.com>
2018-08-22 18:18:55 -0700

    move background images

--- src/resources.js
+++ src/resources.js
@@ -1,9 +1,10 @@
 import player from '../media/img/player.png';
 import ships from '../media/img/ships.png';
 
-import imgTitleScreen from '../media/img/gui/title-screen.png';
-import imgLevelCompletedScreen from '../media/img/gui/level-completed-screen.png';
-import imgGameOverScreen from '../media/img/gui/game-over-screen.png';
+import imgTitleScreen from '../media/img/backgrounds/title-screen.png';
+import imgLevelCompletedScreen from '../media/img/backgrounds/level-completed-screen.png';
+import imgGameOverScreen from '../media/img/backgrounds/game-over-screen.png';
+
 import imgPressStart2P from '/media/fnt/PressStart2P.png';
 import fntPressStart2P from '/media/fnt/PressStart2P.fnt';
 
