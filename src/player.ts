c7f0a28e718b29f72a2f5ef2e3c484f6f3274125
Michael Cohen <michael@planetcohen.com>
2018-08-26 15:26:12 -0700

    clean up player positioning, and laser positioning

--- src/player.ts
+++ src/player.ts
@@ -7,34 +7,39 @@ export const Player = me.Sprite.extend({
   init() {
     // console.log('Player#init');
     var image = me.loader.getImage('player');
-    this._super(me.Sprite, 'init', [
-      me.game.viewport.width / 2 - image.width / 2,
-      me.game.viewport.height - image.height - 20,
-      { image: image },
-    ]);
+
+    const { width: wImage, height: hImage } = image;
+    const { width: wViewport, height: hViewport } = me.game.viewport;
+    const xPos = (wViewport - wImage) / 2;
+    const yPos = hViewport - hImage - 20;
+
+    this._super(me.Sprite, 'init', [xPos, yPos, { image }]);
     this.velx = 450;
-    this.maxX = me.game.viewport.width - this.width;
+    this.xMax = wViewport;
   },
 
-  update(time) {
+  update(dt) {
     // console.log('Player#update');
-    this._super(me.Sprite, 'update', [time]);
+    this._super(me.Sprite, 'update', [dt]);
+
+    const dx = this.velx * dt / 1000;
+
     if (me.input.isKeyPressed('left')) {
-      this.pos.x -= this.velx * time / 1000;
+      this.pos.x -= dx;
     }
 
     if (me.input.isKeyPressed('right')) {
-      this.pos.x += this.velx * time / 1000;
+      this.pos.x += dx;
     }
 
     if (me.input.isKeyPressed('shoot')) {
       me.audio.play(mapSfx.shoot);
       me.game.world.addChild(
-        me.pool.pull('laser', this.pos.x - kLaserWidth, this.pos.y - kLaserHeight)
+        me.pool.pull('laser', this.pos.x - kLaserWidth + 2, this.pos.y - kLaserHeight)
       );
     }
 
-    this.pos.x = this.pos.x.clamp(0, this.maxX);
+    this.pos.x = this.pos.x.clamp(1, this.xMax);
 
     return true;
   },
